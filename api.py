1c637294ce6a43f847cfd27f003fdae7389d2259
sanya <72317007+bunnyyyyy@users.noreply.github.com>
2023-01-21 18:31:35 -0800

    Update api.py

--- api.py
+++ api.py
@@ -2,19 +2,20 @@ import requests
 import json
 import dotenv
 import os
+import csv
 
 dotenv.load_dotenv(".env")
 
 
 def download_image():
-    url = f"https://maps.googleapis.com/maps/api/staticmap?center={37.3013893},{-122.0070246}&key={os.environ.get('API_KEY')}&size={600}x{600}&zoom={18}&maptype=satellite"
+    url = f"https://maps.googleapis.com/maps/api/staticmap?center={400},{-122.0070246}&key={os.environ.get('API_KEY')}&size={600}x{600}&zoom={18}&maptype=satellite"
     response = requests.get(url)
     with open("image.png", "wb") as f:
         f.write(response.content)
 
 
-def get_weather_data():
-    url = " https://api.ambeedata.com/weather/history/by-lat-lng?lat=12&lng=77&from=2023-01-20 00:00:00&to=2023-01-21 00:00:00"
+def get_weather_data(year, month, day, hour, lat, lng):
+    url = f"https://api.ambeedata.com/weather/history/by-lat-lng?lat={lat}&lng={lng}&from={year}-{month}-{day} {hour}:00:00&to={year}-{month}-{day} {hour}:00:00"
     response = requests.get(url, headers={
         "x-api-key": os.environ.get("RAPID_API_KEY"),
         "Content-Type": "application/json"
@@ -23,6 +24,28 @@ def get_weather_data():
     return response.json()
 
 
-print(get_weather_data())
+weather_data = []
+with open("weather_data.txt") as file:
+    lines = file.readlines()[:100] #starting slow
+    for line in lines:
+        numbers = line.split()
+        year, month, day, hour, lat, lng = int(numbers[1]), int(numbers[2]), int(numbers[3]), int(numbers[4]), float(numbers[5]), float(numbers[6])
+        weather_data.append(get_weather_data(year, month, day, hour, lat, lng))
+
+print("loading successful")
+
+
+# commenting so that i dont mess up!
+# def get_weather_data():
+#     url = " https://api.ambeedata.com/weather/history/by-lat-lng?lat=12&lng=77&from=2023-01-20 00:00:00&to=2023-01-21 00:00:00"
+#     response = requests.get(url, headers={
+#         "x-api-key": os.environ.get("RAPID_API_KEY"),
+#         "Content-Type": "application/json"
+#     })
+
+#     return response.json()
+
+
+# print(get_weather_data())
 # download_image()
 # print(get_data())
