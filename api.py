d788a02dcc4b6a523c502b0de4afb36fb9c41e09
snek152 <kakanisnehil@gmail.com>
2023-01-25 10:04:46 -0800

    creating processed file and changing layers yet again

--- api.py
+++ api.py
@@ -17,7 +17,7 @@ def get_weather_data(year: int, month: int, day: int, lat: float, lng: float) ->
     end_date = start_date + datetime.timedelta(days=1)
     url = f"https://archive-api.open-meteo.com/v1/archive?latitude={lat}&longitude={lng}&start_date={start_date.strftime('%Y-%m-%d')}&end_date={end_date.strftime('%Y-%m-%d')}&hourly=temperature_2m"
     response = requests.get(url)
-    print(type(response.json()))
+    # print(type(response.json()))
     return response.json()
 
 
--- dataloader.py
+++ dataloader.py
@@ -1,23 +1,38 @@
 import api
 import scraper
+import datetime
+import operator
 
 
 def data_loader():
-    with open("weather_data.txt") as file:
+    with open("processed_weather_data.txt") as file:
         for line in reversed(file.readlines()):
             numbers = line.split()
-            year, month, day, lat, lng = int(numbers[1]), int(numbers[2]), int(
-                numbers[3]), float(numbers[4]), float(numbers[5])
+            year, month, day, hour, lat, lng, iwv = int(numbers[1]), int(numbers[2]), int(
+                numbers[3]), int(numbers[4]), float(numbers[5]), float(numbers[6])-360, float(numbers[10])
             if year < 2001:
                 break
-            date = f"{year}-{month}-{day}-T00:00:00Z"
+            date = datetime.datetime(year, month, day)
+            # date_string = date.strftime("%Y-%m-%d T%H:%M:%SZ")
             pathname = f"images/image_{year}_{month}_{day}_{lat}_{lng}.png"
+            print(lat, lng, iwv)
             weather_data = api.get_weather_data(year, month, day, lat, lng)
             scraper.save_image(
                 lat, lng, zoom=6.5, date=date, pathname=pathname)
             yield weather_data
 
 
+# with open("weather_data.txt") as file:
+#     with open("processed_weather_data.txt", "w") as file2:
+#         lines = []
+#         for line in file:
+#             numbers = line.split()
+#             year, iwv = int(numbers[1]), float(numbers[10])
+#             if year > 2002:
+#                 lines.append({"year": year, "iwv": iwv, "line": line})
+#         lines.sort(key=operator.itemgetter("iwv", "year"))
+#         for line in lines:
+#             file2.write(line["line"])
 # just learned generator functions!! yayy
 for weather_data in data_loader():
     print(weather_data)
