098a1ddb4c2bc0dc5fa1648a9d26bb2ce2f85a6a
snek152 <kakanisnehil@gmail.com>
2023-01-25 18:02:51 -0800

    setting up csv saver and stuff

--- dataloader.py
+++ dataloader.py
@@ -2,6 +2,12 @@ import api
 import scraper
 import datetime
 import operator
+import pandas as pd
+import cv2
+import os
+from typings import APIResponse
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def data_loader():
@@ -12,14 +18,37 @@ def data_loader():
                 numbers[3]), int(numbers[4]), float(numbers[5]), float(numbers[6])-360, float(numbers[10])
             if year < 2001:
                 break
-            date = datetime.datetime(year, month, day)
-            # date_string = date.strftime("%Y-%m-%d T%H:%M:%SZ")
-            pathname = f"images/image_{year}_{month}_{day}_{lat}_{lng}.png"
+            date = datetime.datetime(year, month, day, hour)
+            date_string = date.strftime("%Y-%m-%d T%H:%M:%SZ")
+            pathname = f"images/image_{year}_{month}_{day}_{hour}_{lat}_{lng}.png"
             print(lat, lng, iwv)
             weather_data = api.get_weather_data(year, month, day, lat, lng)
             scraper.save_image(
-                lat, lng, zoom=6.5, date=date, pathname=pathname)
-            yield weather_data
+                lat, lng, zoom=8, date=date_string, pathname=pathname)
+            yield weather_data, pathname
+
+
+def save_data():
+    df = pd.DataFrame(
+        columns=["image", "lat", "long", "generationtime_ms", "utc_offset_seconds", "timezone", "elevation", "time", "temperature_2m"])
+    with open("processed_weather_data.txt") as file:
+        count = 0
+        for data, image in data_loader():
+            df = df.append({"image": image, "lat": data["latitude"], "long": data["longitude"], "generationtime_ms": data["generationtime_ms"], "utc_offset_seconds": data["utc_offset_seconds"],
+                           "timezone": data["timezone"], "elevation": data["elevation"], "time": data["hourly"]["time"], "temperature_2m": data["hourly"]["temperature_2m"]}, ignore_index=True)
+            count += 1
+            if count >= 2:
+                break
+    df.to_csv("data.csv")
+
+
+def show_data():
+    df = pd.read_csv("data.csv")
+    for index, row in df.iterrows():
+        image = cv2.imread(row["image"])
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        plt.imshow(image)
+        plt.show()
 
 
 # with open("weather_data.txt") as file:
@@ -34,5 +63,7 @@ def data_loader():
 #         for line in lines:
 #             file2.write(line["line"])
 # just learned generator functions!! yayy
-for weather_data in data_loader():
-    print(weather_data)
+# for weather_data in data_loader():
+#     print(weather_data)
+save_data()
+show_data()
--- scraper.py
+++ scraper.py
@@ -11,7 +11,7 @@ from selenium.webdriver.common.by import By
 def crop_image(pathname: str):
     im = Image.open(pathname)
     width, height = im.size
-    crop_radius = 200
+    crop_radius = 300
     left = width//2 - crop_radius
     right = width//2 + crop_radius
     top = height//2 - crop_radius
@@ -22,13 +22,13 @@ def crop_image(pathname: str):
 
 def save_image(lat, long, zoom, date, pathname):
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    url = f"https://worldview.earthdata.nasa.gov/?v={long-zoom},{lat-zoom},{long+zoom},{lat+zoom}&l=Reference_Labels_15m(hidden),Reference_Features_15m(hidden),Coastlines_15m(hidden),AMSRE_Columnar_Water_Vapor_Day,AMSRU2_Columnar_Water_Vapor_Day,AMSRE_Columnar_Water_Vapor_Night,AMSRU2_Columnar_Water_Vapor_Night,AMSRU2_Total_Precipitable_Water_Night,AMSRU2_Total_Precipitable_Water_Day&lg=false&t={date}"
+    url = f"https://worldview.earthdata.nasa.gov/?v={long-zoom},{lat-zoom},{long+zoom},{lat+zoom}&l=Reference_Labels_15m(hidden),Reference_Features_15m(hidden),Coastlines_15m(hidden),AMSRU2_Total_Precipitable_Water_Night,AMSRU2_Total_Precipitable_Water_Day,AMSRE_Columnar_Water_Vapor_Day,AMSRU2_Columnar_Water_Vapor_Day,AMSRE_Columnar_Water_Vapor_Night,AMSRU2_Columnar_Water_Vapor_Night&lg=false&t={date}"
     # other_url = "https://worldview.earthdata.nasa.gov/?v=-340.5066279931091,-142.6995676849225,236.2386721655414,209.52537981404555&l=Reference_Labels_15m(hidden),Reference_Features_15m(hidden),Coastlines_15m(hidden),AMSRE_Columnar_Water_Vapor_Day,AMSRU2_Columnar_Water_Vapor_Day,AMSRE_Columnar_Water_Vapor_Night,AMSRU2_Columnar_Water_Vapor_Night,AMSRU2_Total_Precipitable_Water_Night,AMSRU2_Total_Precipitable_Water_Day,BlueMarble_NextGeneration(hidden),VIIRS_NOAA20_CorrectedReflectance_TrueColor(hidden),VIIRS_SNPP_CorrectedReflectance_TrueColor(hidden),MODIS_Aqua_CorrectedReflectance_TrueColor(hidden),MODIS_Terra_CorrectedReflectance_TrueColor(hidden)&lg=true&t=2013-02-21-T20%3A00%3A00Z"
     # other_url = "https://worldview.earthdata.nasa.gov/?v=-125.3095952625997,-57.724467407598226,154.42275900064254,113.11127892103254&l=Reference_Labels_15m(hidden),Reference_Features_15m(hidden),Coastlines_15m(hidden),AMSRU2_Columnar_Water_Vapor_Night,AMSRU2_Columnar_Water_Vapor_Day,BlueMarble_NextGeneration(hidden),VIIRS_NOAA20_CorrectedReflectance_TrueColor(hidden),VIIRS_SNPP_CorrectedReflectance_TrueColor(hidden),MODIS_Aqua_CorrectedReflectance_TrueColor(hidden),MODIS_Terra_CorrectedReflectance_TrueColor(hidden)&lg=true&t=2023-01-25-T16%3A51%3A37Z"
     # other_url = "https://worldview.earthdata.nasa.gov/?v=-175.19751452291467,2.0849570200676837,-37.90523544049556,85.93092122387918&z=2&l=Reference_Labels_15m(hidden),Reference_Features_15m(hidden),Coastlines_15m(hidden),MODIS_Terra_Water_Vapor_5km_Night,MODIS_Terra_Water_Vapor_5km_Day,VIIRS_NOAA20_CorrectedReflectance_TrueColor(hidden),VIIRS_SNPP_CorrectedReflectance_TrueColor,MODIS_Aqua_CorrectedReflectance_TrueColor,MODIS_Terra_CorrectedReflectance_TrueColor(hidden)&lg=true&t=2000-11-14-T20%3A53%3A35Z"
     # other_url = "https://worldview.earthdata.nasa.gov/?v=-128.9,-37.3,-128.7,-37.2&l=Reference_Labels_15m(hidden),Reference_Features_15m(hidden),Coastlines_15m,Land_Water_Map,MODIS_Combined_MAIAC_L2G_ColumnWaterVapor,VIIRS_SNPP_CorrectedReflectance_TrueColor(hidden),MODIS_Aqua_CorrectedReflectance_TrueColor(hidden),MODIS_Terra_CorrectedReflectance_TrueColor(hidden)&lg=false&t=2019-12-29-T07:53:06Z"
     driver.get(url)
-    time.sleep(10)
+    time.sleep(5)
     driver.find_element(By.ID, "toggleIconHolder").click()
     driver.find_element(By.ID, "timeline-hide").click()
     driver.find_element(By.ID, "wv-map").screenshot(pathname)
