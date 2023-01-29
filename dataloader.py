40ea31038a71e0ac672631a9eea8df005a52f760
snek152 <kakanisnehil@gmail.com>
2023-01-28 16:01:40 -0800

    testing changes for random generating of data

--- dataloader.py
+++ dataloader.py
@@ -10,9 +10,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def random_lat_lon(n=1, lat_min=-90., lat_max=90., lon_min=-180., lon_max=180.):
+    """
+    this code produces an array with pairs lat, lon
+    """
+    lat = np.random.uniform(lat_min, lat_max, n)
+    lon = np.random.uniform(lon_min, lon_max, n)
+
+    return np.array(tuple(zip(lat, lon)))
+
+
 def data_loader():
+    lineCount = 0
     with open("processed_weather_data.txt") as file:
-        for line in reversed(file.readlines()):
+        for line in file:
+            lineCount += 1
             numbers = line.split()
             year, month, day, hour, lat, lng, iwv = int(numbers[1]), int(numbers[2]), int(
                 numbers[3]), int(numbers[4]), float(numbers[5]), float(numbers[6])-360, float(numbers[10])
@@ -25,17 +37,29 @@ def data_loader():
             weather_data = api.get_weather_data(year, month, day, lat, lng)
             scraper.save_image(
                 lat, lng, zoom=8, date=date_string, pathname=pathname)
-            yield weather_data, pathname
+            yield weather_data, pathname, iwv
+    # INCOMPLETE STUFF
+    # with open("processed_weather_data.txt") as file:
+    #     i = 0
+    #     while i < lineCount:
+    #         lat, long = tuple(zip(np.random.uniform(-90., 90., 1),
+    #                               np.random.uniform(-180., 180., 1)))[0]
+    #         iwv = np.random.uniform(0, 100)
+    #         year = np.random.randint(2001, 2021)
+    #         month = np.random.randint(1, 13)
+    #         day = np.random.randint(1, 29)
+    #         hour = np.random.randint(0, 24)
+    #         date = datetime.datetime(year, month, day, hour)
 
 
 def save_data():
     df = pd.DataFrame(
-        columns=["image", "lat", "long", "generationtime_ms", "utc_offset_seconds", "timezone", "elevation", "time", "temperature_2m"])
+        columns=["image", "lat", "long", "generationtime_ms", "utc_offset_seconds", "timezone", "elevation", "time", "temperature_2m", "iwv", "ar"])
     with open("processed_weather_data.txt") as file:
         count = 0
-        for data, image in data_loader():
+        for data, image, iwv in data_loader():
             df = df.append({"image": image, "lat": data["latitude"], "long": data["longitude"], "generationtime_ms": data["generationtime_ms"], "utc_offset_seconds": data["utc_offset_seconds"],
-                           "timezone": data["timezone"], "elevation": data["elevation"], "time": data["hourly"]["time"], "temperature_2m": data["hourly"]["temperature_2m"]}, ignore_index=True)
+                            "timezone": data["timezone"], "elevation": data["elevation"], "time": data["hourly"]["time"], "temperature_2m": data["hourly"]["temperature_2m"], "iwv": iwv, "ar": 1}, ignore_index=True)
             count += 1
             if count >= 2:
                 break
