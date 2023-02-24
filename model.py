d22a21e7557653c25e47924d10fc37bc9d5fed17
snek152 <kakanisnehil@gmail.com>
2023-02-23 16:04:20 -0800

    making some model changes ig

--- model.py
+++ model.py
@@ -1,21 +1,24 @@
-import os
-from os import listdir
-from PIL import Image
-import torch
-import torch.nn as nn
-import numpy as np
-import torch.nn.functional as F
-import time
-from torch.utils.data.dataset import Dataset
-from torch.utils.data.dataloader import DataLoader
-import sklearn.metrics as metrics
-from sklearn.datasets import make_blobs
-import matplotlib.pyplot as plt
-from torchvision import transforms
-import torchvision
-from tqdm import tqdm
-from torch import device
+import gc
 import pandas as pd
+from torch import device
+from tqdm import tqdm
+import torchvision
+from torchvision import transforms
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+import sklearn.metrics as metrics
+from torch.utils.data.dataloader import DataLoader
+from torch.utils.data.dataset import Dataset
+import time
+import torch.nn.functional as F
+import numpy as np
+import torch.nn as nn
+import torch
+from PIL import Image
+from os import listdir
+import os
+
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:128"
 
 
 # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -78,12 +81,12 @@ class MyDataset(Dataset):
 
 
 my_dataset = MyDataset(pd.read_pickle("train_data.pkl"), device)
-my_dataloader = DataLoader(my_dataset, batch_size=64,
-                           shuffle=True, num_workers=0, collate_fn=my_dataset.collate_fn)
+my_dataloader = DataLoader(my_dataset, batch_size=8,
+                           shuffle=False, num_workers=0, collate_fn=my_dataset.collate_fn)
 
 my_valid_dataset = MyDataset(pd.read_pickle("val_data.pkl"), device)
-validloader = DataLoader(my_valid_dataset, batch_size=32,
-                         shuffle=True, num_workers=0, collate_fn=my_valid_dataset.collate_fn)
+validloader = DataLoader(my_valid_dataset, batch_size=8,
+                         shuffle=False, num_workers=0, collate_fn=my_valid_dataset.collate_fn)
 
 
 # cnn = torchvision.models.resnet18(weights=None, num_classes=1)
@@ -141,142 +144,200 @@ class Model(nn.Module):
 # print(cnn)
 
 
-cnn = Model(num_classes=2, num_weather_features=1).to(device)
-optimizer = torch.optim.SGD(cnn.parameters(), lr=0.01)
-loss_func = nn.CrossEntropyLoss()
-# cnn.train()
-
-
-best_acc = 0.0
-
-for epoch in range(30):
-    print("EPOCH # " + str(epoch))
-
-    total = 0
-
-    for batch in tqdm(my_dataloader):
-        optimizer.zero_grad()
-
-        # forward pass!
-        inp, weather_data, labels = batch
-        inp, weather_data, labels = inp.to(
-            device), weather_data.to(device), labels.to(device)
-
-        out = cnn(inp, weather_data)
-        # labels = labels.view(-1, 1)  # reshapes labels to fit properly!
-
-        # apparently we need this idk
-        labels = labels.long()
-
-        loss = loss_func(out, labels)
-
-        # backward pass!!
-        loss.backward()
-        optimizer.step()
-
-        total += 1
-        print('[%d, %5d] loss: %.3f' % (epoch + 1, total, loss.item()))
-
-    # val using validation dataset
-    # valid_loss = 0.0
-    # cnn.eval()     # optional on non model-specific layers aka ours
-    valid_correct = 0
-    valid_total = 0
-    with torch.no_grad():
-        for data in validloader:
-            inp, weather_data, labels = data
-            inp = inp.to(device)
-            weather_data = weather_data.to(device)
-            labels = labels.to(device)
-
-            target = cnn(inp, weather_data)
-            # labels = labels.view(-1, 1)  # Reshape labels to [batch_size, 1]
-            # lo = loss_func(target, labels.float())
-            # valid_sum += lo
-            # valid_total += 1
-            _, predicted = torch.max(target.data, 1)
-            valid_total += labels.size(0)
-            valid_correct += (predicted == labels).sum().item()
-
+def train_model():
+    cnn = Model(num_classes=2, num_weather_features=1).to(device)
+    optimizer = torch.optim.SGD(cnn.parameters(), lr=0.01)
+    loss_func = nn.CrossEntropyLoss()
     cnn.train()
 
-    # print(loss_sum / total)
-    # valid_loss_avg = valid_sum / valid_total
-    # print(valid_loss_avg)
-    # if (valid_loss_avg < min_avg_loss):
-    #     min_avg_loss = valid_loss_avg
-    #     torch.save(cnn, 'ay.pt')
-    acc = 100 * valid_correct / valid_total
-    print('Accuracy on the validation images: %d %%' % acc)
+    gc.collect()
+    torch.cuda.empty_cache()
 
-    if acc > best_acc:
-        best_accuracy = acc
-        torch.save(cnn.state_dict(), 'best_cnn.pt')
+    best_acc = 0.0
+    train_losses = []
+    val_losses = []
+
+    for epoch in range(2):
+        gc.collect()
+        torch.cuda.empty_cache()
+        print("EPOCH # " + str(epoch))
+
+        total = 0
+        t_losses = []
+        v_losses = []
+
+        for batch in my_dataloader:
+            gc.collect()
+            torch.cuda.empty_cache()
+            optimizer.zero_grad()
+
+            # forward pass!
+            inp, weather_data, labels = batch
+            inp, weather_data, labels = inp.to(
+                device), weather_data.to(device), labels.to(device)
+
+            out = cnn(inp, weather_data)
+            # labels = labels.view(-1, 1)  # reshapes labels to fit properly!
+
+            # apparently we need this idk
+            labels = labels.long()
+
+            loss = loss_func(out, labels)
+
+            # backward pass!!
+            loss.backward()
+            optimizer.step()
+
+            total += 1
+            t_losses.append(loss.item())
+            print('[%d, %5d] loss: %.3f' % (epoch + 1, total, loss.item()))
+
+        train_losses.append(sum(t_losses)/len(t_losses))
+        # val using validation dataset
+        # valid_loss = 0.0
+        gc.collect()
+        torch.cuda.empty_cache()
+        cnn.eval()     # optional on non model-specific layers aka ours
+        valid_correct = 0
+        valid_total = 0
+        with torch.no_grad():
+            for data in validloader:
+                gc.collect()
+                torch.cuda.empty_cache()
+                inp, weather_data, labels = data
+                inp = inp.to(device)
+                weather_data = weather_data.to(device)
+                labels = labels.to(device)
+
+                target = cnn(inp, weather_data)
+                # labels = labels.view(-1, 1)  # Reshape labels to [batch_size, 1]
+                lo = loss_func(target, labels.long())
+                v_losses.append(lo.item())
+                # valid_sum += lo
+                # valid_total += 1
+                _, predicted = torch.max(target.data, 1)
+                valid_total += labels.size(0)
+                valid_correct += (predicted == labels).sum().item()
+        val_losses.append(sum(v_losses)/len(v_losses))
+        cnn.train()
+        gc.collect()
+        torch.cuda.empty_cache()
+
+        # print(loss_sum / total)
+        # valid_loss_avg = valid_sum / valid_total
+        # print(valid_loss_avg)
+        # if (valid_loss_avg < min_avg_loss):
+        #     min_avg_loss = valid_loss_avg
+        #     torch.save(cnn, 'ay.pt')
+        acc = 100 * valid_correct / valid_total
+        print('Accuracy on the validation images: %d %%' % acc)
+
+        if acc > best_acc:
+            best_accuracy = acc
+            torch.save(cnn.state_dict(), 'test_cnn.pt')
+
+    print(train_losses, val_losses)
+    epochs = range(1, len(train_losses) + 1)
+
+    plt.plot(epochs, train_losses, 'g', label='Avg training loss')
+    plt.plot(epochs, val_losses, 'b', label='Avg validation loss')
+    plt.title('Training and validation loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
+
+    network_answers = []
+    true_answers = []
+    cnn = cnn.eval()
+
+    right = 0
+    total = 0
+    wrong = 0
+
+    net_pred = []
+    true_pred = []
+    for inp, weather_data, labels in my_dataloader:
+        inp, weather_data, labels = inp.to(
+            device), weather_data.to(device), labels.to(device)
+        with torch.no_grad():
+            out = cnn(inp, weather_data)
+
+            sigmoid_layer = torch.nn.Sigmoid()
+            predicted = sigmoid_layer(out).cpu().numpy() > 0.5
+        #  predicted = torch.nn.Sigmoid(out, dim=-1).cpu().numpy() > 0.5
+            actual = labels.cpu().numpy() > 0.5
+            net_pred.append(predicted)
+            true_pred.append(actual)
+
+    y_pred = (net_pred)[0]
+    y_true = (true_pred)[0]
+    acc = 0
+
+    correct = 0
+    total = 0
+    true_pos = 0
+    false_pos = 0
+    true_neg = 0
+    false_neg = 0
+    # print((y_true[0]))
+    # print(y_pred[0])
+    # print(len(y_true))
+    # print(data["test_labels"])
+
+    # y_true = 0 < data["test_labels"]
+    print(len(y_true))
+    print(len(y_pred))
+    for i in range(len(y_true)):
+        pred = np.asarray(y_pred[i])
+        true = np.asarray(y_true[i])
+        for j in range(pred.shape[0]):
+            if pred[j] == true[j] == True:
+                true_pos += 1
+            elif pred[j] == true[j] == False:
+                true_neg += 1
+            elif pred[j] == False and true[j] == True:
+                false_neg += 1
+            elif pred[j] == True and true[j] == False:
+                false_pos += 1
+
+    print(true_pos, false_pos, true_neg, false_neg)
+    correct = (true_pos + true_neg) / \
+        (true_neg + false_neg + true_pos + false_pos)
+    print("Accuracy: " + str(correct))
+    precision = true_pos / (true_pos + false_pos)
+    print("precision: " + str(precision))
+    recall = true_pos / (true_pos + false_neg)
+    print("recall: " + str(recall))
+    f1_score = 2 * precision * recall / (precision + recall)
+    print("f1_score: " + str(f1_score))
+
+    labels = ['atmospheric river present']
+
+    print(metrics.classification_report(y_true, y_pred, target_names=labels))
 
 
-network_answers = []
-true_answers = []
-cnn = cnn.eval()
+def predict_data(image_path: str, temp: float, humidity: float, dewpoint: float, precipitation: float):
+    my_model = Model(2, 1)
+    my_model.load_state_dict(torch.load("best_cnn.pt"))
+    df = pd.DataFrame(columns=["image", "temperature",
+                               "humidity", "dewpoint", "precipitation", "ar"])
+    df = df.append({"image": image_path, "temperature_2m": temp, "humidity": humidity,
+                   "dewpoint": dewpoint, "precipitation": precipitation, "ar": 0}, ignore_index=True)
+    dataset = MyDataset(df, device=torch.device("cpu"))
+    loader = DataLoader(dataset, batch_size=1,
+                        shuffle=False, num_workers=0, collate_fn=dataset.collate_fn)
+    labels = [False, True]
+    for data in loader:
+        print(data)
+        inp, weather_data, _ = data
+        outputs = my_model(inp, weather_data)
+        print(outputs)
+        _, predicted = torch.max(outputs.data, 1)
+        predicted = predicted
+        return labels[predicted[0]]
 
-right = 0
-total = 0
-wrong = 0
 
-net_pred = []
-true_pred = []
-for inp, weather_data, labels in my_dataloader:
-
-    with torch.no_grad():
-        out = cnn(inp, weather_data)
-
-        sigmoid_layer = torch.nn.Sigmoid()
-        predicted = sigmoid_layer(out).cpu().numpy() > 0.5
-       #  predicted = torch.nn.Sigmoid(out, dim=-1).cpu().numpy() > 0.5
-        actual = labels.cpu().numpy() > 0.5
-        net_pred.append(predicted)
-        true_pred.append(actual)
-
-y_pred = (net_pred)[0]
-y_true = (true_pred)[0]
-acc = 0
-
-correct = 0
-total = 0
-true_pos = 0
-false_pos = 0
-true_neg = 0
-false_neg = 0
-# print((y_true[0]))
-# print(y_pred[0])
-# print(len(y_true))
-# print(data["test_labels"])
-
-# y_true = 0 < data["test_labels"]
-print(len(y_true))
-print(len(y_pred))
-for i in range(len(y_true)):
-    pred = np.asarray(y_pred[i])
-    true = np.asarray(y_true[i])
-    for j in range(pred.shape[0]):
-        if pred[j] == true[j] == True:
-            true_pos += 1
-        elif pred[j] == true[j] == False:
-            true_neg += 1
-        elif pred[j] == False and true[j] == True:
-            false_neg += 1
-        elif pred[j] == True and true[j] == False:
-            false_pos += 1
-
-print(true_pos, false_pos, true_neg, false_neg)
-correct = (true_pos + true_neg) / (true_neg + false_neg + true_pos + false_pos)
-print("Accuracy: " + str(correct))
-precision = true_pos / (true_pos + false_pos)
-print("precision: " + str(precision))
-recall = true_pos / (true_pos + false_neg)
-print("recall: " + str(recall))
-f1_score = 2 * precision * recall / (precision + recall)
-print("f1_score: " + str(f1_score))
-
-labels = ['atmospheric river present']
-
-print(metrics.classification_report(y_true, y_pred, target_names=labels))
+# print(predict_data("images/image_2003_1_8_6_27.5_-112.5.png", 20.0, 2.0, 2.0, 2.0))
+if __name__ == "__main__":
+    train_model()
--- server.py
+++ server.py
@@ -5,17 +5,22 @@ import os
 from api import get_weather_data
 from scraper import save_image
 import datetime
+from model import predict_data
 
 app = FastAPI()
 
 
 @app.get("/")
-async def home(lat: float, long: float, year: int, month: int, day: int, hour: int):
+# lat: float, long: float, year: int, month: int, day: int, hour: int):
+async def home():
     # this is all boilerplate btw none of this is gonna work
-    model = torch.load("model.pkl")
-    weather_data = get_weather_data(year, month, day, lat, long)
-    date = datetime.datetime(year, month, day, hour)
-    date_string = date.strftime("%Y-%m-%d T%H:%M:%SZ")
-    image = save_image(lat, long, 8, date_string, "image.png")
+    # model = torch.load("model.pkl")
+    # weather_data = get_weather_data(year, month, day, lat, long)
+    # date = datetime.datetime(year, month, day, hour)
+    # date_string = date.strftime("%Y-%m-%d T%H:%M:%SZ")
+    # image = save_image(lat, long, 8, date_string, "image.png")
     # model predict using weather data and image (will figure out how to do this after u build model so we can use the right parameters and stuff)
-    return {"message": "Hello World"}
+    return {"ar": predict_data("images/image_2003_1_8_6_27.5_-112.5.png", 0.0, 2.0, 2.0, 2.0)}
+
+if __name__ == "__main__":
+    uvicorn.run(app, reload=True)
